4e1894790e2946c1c2c228e4c38edbee91d46f75
Dave Nicponski <dave.nicponski@gmail.com>
2020-08-27 14:31:01 -0400

    Utility methods on Iterable - typesafe partitioning
    Rather than partition using `Seq.partition`, which leaves
    the original type `A` in place on both sides, it's very
    useful to have a means of mapping into one of two different
    types, and having that reflected in the result types of the
    two iterables produced.

---
+++ virusdave/ops/IterableOps.scala
@@ -0,0 +1,34 @@
+package virusdave.ops
+
+import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable
+import scala.language.higherKinds
+
+trait IterableOps {
+  implicit class _IterableOps[Z, Collection[+Element] <: Iterable[Element]](private val in: Collection[Z]) {
+
+    // This one at least keeps the mutability hidden
+    def partitionWith[L, R](fn: Z => Either[L, R])(
+        implicit cbf1: CanBuildFrom[Nothing, L, Collection[L]],
+        cbf2: CanBuildFrom[Nothing, R, Collection[R]]): (Collection[L], Collection[R]) = {
+      val lhs = new mutable.ArrayBuffer[L](in.size)
+      val rhs = new mutable.ArrayBuffer[R](in.size)
+      in foreach fn.andThen {
+        case Left(v) => lhs += v
+        case Right(v) => rhs += v
+      }
+      (lhs.to[Collection], rhs.to[Collection])
+    }
+
+    // This one is more 'pure', but almost certainly less efficient.
+    def _partitionWith2[L, R](fn: Z => Either[L, R])(
+        implicit cbf1: CanBuildFrom[Nothing, L, Collection[L]],
+        cbf2: CanBuildFrom[Nothing, R, Collection[R]]): (Collection[L], Collection[R]) = {
+      val (lhs, rhs) = (in: Iterable[Z]).map(fn).map {
+        case Left(v) => (Seq(v), Seq.empty[R])
+        case Right(v) => (Seq.empty[L], Seq(v))
+      }.unzip
+      (lhs.flatten.to[Collection], rhs.flatten.to[Collection])
+    }
+  }
+}
